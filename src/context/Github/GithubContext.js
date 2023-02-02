d1e9243475df4ecdc869646bc106aec4d3cde8de
KikoB10 <burten.kiko@gmail.com>
2023-02-02 15:21:59 -0600

    come back to to fix : error: cannot find file: 'GithubActions.js' does not match the corresponing name on disk: './src/context/github/Github

--- src/context/Github/GithubContext.js
+++ src/context/Github/GithubContext.js
@@ -3,8 +3,6 @@ import githubReducer from './GithubReducer';
 import React from 'react';
 
 const GithubContext = createContext();
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
   const initialState = {
@@ -15,68 +13,11 @@ export const GithubProvider = ({ children }) => {
   };
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  //Get a single user
-  const getUser = async (login) => {
-    setLoading();
-
-    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
-
-    if (response.status === 404) {
-      window.location = '/notfound';
-    } else {
-      const data = await response.json();
-
-      dispatch({
-        type: 'GET_USER',
-        payload: data,
-      });
-    }
-  };
-
-  //Get user Repos
-  const getUserRepos = async (login) => {
-    setLoading();
-
-    const params = new URLSearchParams({
-      sort: 'created',
-      per_page: 10,
-    });
-    const response = await fetch(
-      `${GITHUB_URL}/users/${login}/repos?${params}`,
-      {
-        headers: {
-          Authorization: `token ${GITHUB_TOKEN}`,
-        },
-      }
-    );
-
-    const data = await response.json();
-
-    dispatch({
-      type: 'GET_REPOS',
-      payload: data,
-    });
-  };
-
-  //clear users from state
-  const clearUsers = () => dispatch({ type: 'CLEAR_USERS' });
-
-  //Set loading
-  const setLoading = () => dispatch({ type: 'SET_LOADING' });
-
   return (
     <GithubContext.Provider
       value={{
         ...state,
         dispatch,
-
-        clearUsers,
-        getUser,
-        getUserRepos,
       }}
     >
       {children}
